4cc3d52b4f0ddc333c8ab600a6535a543426f0a3
Todd Hayes <todd@i.tv>
2015-09-22 15:05:50 -0600

    finished Home, Complete and Admin Pages

--- js/admin/loginCtrl.js
+++ js/admin/loginCtrl.js
@@ -1,20 +1,5 @@
 var lowesCpn = angular.module('lowesCpn');
 	
-	// lowesCpn.controller('loginCtrl', function($scope, $location, $firebaseAuth){
-	// 	$scope.user = {};
-	// 	$scope.authRef.$authWithPassword({
-  	// 	 email: "user.email",
- 	// 	 password: "user.password"
-	// 	}).then(function(authData) {
-  	// 	console.log("Logged in as:", authData.uid);
-	// 	}).catch(function(error) {
-  	// 	console.error("Authentication failed:", error);
-	// 	});
-	
-		
-	// });
-	
-	
 	lowesCpn.controller('loginCtrl', function($scope, $location, $firebaseAuth){
 		var firebaseObj = new Firebase('https://lowes-coupon.firebaseio.com/');
 		var loginObj = $firebaseAuth(firebaseObj);
@@ -24,17 +9,12 @@ var lowesCpn = angular.module('lowesCpn');
 			loginObj.$authWithPassword({
            		 email: username,
 				 password: password
-        }, function(error, authData) {
-            //Failure callback
-			if(error){
-            console.log('Authentication failure');
-			alert('wrong password or username. Please try again');
-		} else {
-			console.log('Authentication successful', authData);
+        }).then(function(user){
+			console.log('Authentication successful');
 			$location.path('/admin');
-		}
+		}, function(user){
+			console.log('Authentication failure');
+			alert('wrong password or username. Please try again');
 		})
-		
-		
 		}
 	});
--- js/directive/appDirective.js
+++ js/directive/appDirective.js
@@ -8,7 +8,7 @@ lowesCpn.directive('navbar', function(){
 
 lowesCpn.directive('returnHome', function(){
 	return {
-		template: '<div><a href="#/"><button>Start Over</button></a></div>'
+		template: '<div><a href="#/"><button class="glyphicon glyphicon-home" aria-hidden="true"></button></a></div>'
 	};
 })
 
@@ -18,44 +18,3 @@ lowesCpn.directive('customFooter', function(){
 	}
 });
 
-lowesCpn.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' + 
-          '<div class="modal-dialog">' + 
-            '<div class="modal-content">' + 
-              '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ title }}</h4>' + 
-              '</div>' + 
-              '<div class="modal-body" ng-transclude></div>' + 
-            '</div>' + 
-          '</div>' + 
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
-
-        scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
-
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
-
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
